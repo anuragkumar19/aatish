bde48e290c8b0761886817986a905e9af6c19a9a
Anurag Kumar <65001074+anuragkumar19@users.noreply.github.com>
2024-03-08 07:56:38 +0000

    Pending changes exported from your codespace

---
+++ src/aatish.ts
@@ -0,0 +1,22 @@
+export class Aatish {
+    constructor() {}
+}
+
+/**
+ * Aatish
+ *
+ * const app = new Aatish({
+ *  connect(){
+ *
+ * }
+ * })
+ *
+ * app.use(middleware())
+ *
+ * app.on("event", (data, user,cb) => {
+ *
+ *  return cb()
+ * })
+ *
+ * app.emit()
+ */
